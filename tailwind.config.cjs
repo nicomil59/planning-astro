2085868172648d8b1a8433b36747ff117c92ec14
Nicolas Milhem <nicolasmilhem@gmail.com>
2023-09-21 15:18:24 +0200

    💄 Add custom font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				title: 'Oswald, sans-serif', // Adds a new `font-display` class
+			}
+		},
 	},
 	plugins: [],
 }
