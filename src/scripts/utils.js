ba1b70c920fb1cbe23f028adba46eb165bc7d0c0
Nicolas Milhem <nicolasmilhem@gmail.com>
2023-09-25 17:53:51 +0200

    🚧 PLANNING > Display shows by date

---
+++ src/scripts/utils.js
@@ -0,0 +1,55 @@
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+import advancedFormat from "dayjs/plugin/advancedFormat";
+import "dayjs/locale/fr";
+
+dayjs.extend(customParseFormat);
+dayjs.extend(advancedFormat);
+dayjs.locale("fr");
+
+// ***** Fonctions pour gérer les dates *****
+
+// Fonction qui récupère date en string au format "25/08/2023" et retourne au format "vendredi 25 août 2023"
+
+export const formatDate = (dateStr) => {
+  const dateObj = dayjs(dateStr, "DD/MM/YYYY");
+  return dateObj.format("ddd Do MMM YYYY");
+};
+
+// Fonction qui génère un tableau d'objets regroupant les séries TV par date de diffusion
+
+export const getShowsByDate = (data) => {
+  let showsByDate = {};
+  data.forEach((show) => {
+    let date = show.Date;
+    if (!showsByDate[date]) {
+      showsByDate[date] = [];
+    }
+    showsByDate[date].push(show);
+  });
+
+  const keys = Object.keys(showsByDate);
+  const values = Object.values(showsByDate);
+
+  const showsByDateFormatted = keys.map((key) => {
+    return {
+      date: [key][0],
+      shows: values[keys.indexOf(key)],
+    };
+  });
+
+  return showsByDateFormatted;
+};
+
+// Fonction de comparaison pour trier les dates (avec dayjs)
+const compareDates = (a, b) => {
+  const date1 = dayjs(a.date, "DD/MM/YYYY");
+  const date2 = dayjs(b.date, "DD/MM/YYYY");
+  return date1 - date2;
+};
+
+// Fonction qui permet de trier des dates de diffusion par ordre chronologique à partir d'un objet regroupant des séries par date de diffusion
+
+export const getSortedShowsByDate = (objDates) => {
+  return objDates.sort(compareDates);
+};
