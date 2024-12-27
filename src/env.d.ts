d7d48588fa8b03bfd30891c83ef1b1ced3df163e
Nicolas Milhem <nicolasmilhem@gmail.com>
2024-12-27 14:15:48 +0100

    ✨ Implement a cookie consent system

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+
+interface Window {
+    cookieconsent: any;
+    gtag: any;
+    dataLayer: any;
+  }
+  
+  declare const cookieconsent: any;
+  declare function gtag(...args: any[]): void;
